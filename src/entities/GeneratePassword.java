f025b1d46190e570c826348a54fbc5dffd1f7ed5
Willaim-dias <williampacifico41@gmail.com>
2024-12-29 18:15:42 -0300

    criacao da funcao para gerar a senha

---
+++ src/entities/GeneratePassword.java
@@ -0,0 +1,86 @@
+package entities;
+
+import java.util.Random;
+
+public class GeneratePassword {
+
+    private final String[] ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");//[0]
+    private final String[] abc = "abcdefghijklmnopqrstuvwxyz".split("");//[1]
+    private final String[] number = "0123456789".split("");//[2]
+    private final String[] character = "!@#$%^&*()-_=+[]{};:'\"<>.,/?\\|`~".split("");//[3]
+
+    private final Random random = new Random();
+
+    public GeneratePassword() {
+    }
+
+    public String passwordStandard() {
+        StringBuilder password = new StringBuilder();
+        password.append(ABC[random.nextInt(ABC.length)]);
+        password.append(abc[random.nextInt(abc.length)]);
+        password.append(number[random.nextInt(number.length)]);
+        password.append(character[random.nextInt(character.length)]);
+
+        String[] charactersRemaining = (String.join("", ABC) + String.join("", abc) + String.join("", number) + String.join("", character)).split("");
+
+        for (int i = 0; i < 4; i++) {
+            password.append(charactersRemaining[random.nextInt(charactersRemaining.length)]);
+        }
+
+        char[] shuffle = password.toString().toCharArray();
+
+        for (int i = 0; i < shuffle.length; i++) {
+            int position = random.nextInt(shuffle.length);
+            char temp = shuffle[position];
+            shuffle[i] = shuffle[position];
+            shuffle[position] = temp;
+        }
+        return new String(password);
+    }
+
+    public String passwordWithoutkey(boolean[] option, int size) {
+        boolean isTrue = false;
+        for (boolean b : option) {
+            if (b) {
+                isTrue = true;
+            }
+        }
+        if (isTrue) {
+            StringBuilder password = new StringBuilder();
+            StringBuilder charactersPool = new StringBuilder();
+
+            if (option[0]) {
+                password.append(ABC[random.nextInt(ABC.length)]);
+                charactersPool.append(String.join("", ABC));
+            }
+            if (option[1]) {
+                password.append(abc[random.nextInt(abc.length)]);
+                charactersPool.append(String.join("", abc));
+            }
+            if (option[2]) {
+                password.append(number[random.nextInt(number.length)]);
+                charactersPool.append(String.join("", number));
+            }
+            if (option[3]) {
+                password.append(character[random.nextInt(character.length)]);
+                charactersPool.append(String.join("", character));
+            }
+
+            String[] poolArrry = charactersPool.toString().split("");
+            for (int i = 0; i < size; i++) {
+                password.append(poolArrry[random.nextInt(poolArrry.length)]);
+            }
+
+            char[] shuffle = password.toString().toCharArray();
+
+            for (int i = 0; i < shuffle.length; i++) {
+                int position = random.nextInt(shuffle.length);
+                char temp = shuffle[position];
+                shuffle[i] = shuffle[position];
+                shuffle[position] = temp;
+            }
+            return new String(shuffle);
+        }
+        return passwordStandard();
+    }
+}
--- src/view/HomeController.java
+++ src/view/HomeController.java
@@ -1,36 +1,33 @@
 package view;
 
+import entities.GeneratePassword;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 
 public class HomeController implements Initializable {
 
-    @FXML
-    private Button btCreate;
+    private final GeneratePassword generate = new GeneratePassword();
 
     @FXML
-    private RadioButton rb123;
+    private RadioButton rbABC;//[0]
 
     @FXML
-    private RadioButton rbABC;
+    private RadioButton rbabc;//[1]
 
     @FXML
-    private RadioButton rbCharacters;
+    private RadioButton rb123;//[2]
 
     @FXML
-    private RadioButton rbKey;
+    private RadioButton rbCharacters;//[3]
 
     @FXML
-    private RadioButton rbabc;
+    private RadioButton rbActiveKey;//Em dev..
 
     @FXML
     private Slider sliderNumber;
@@ -39,26 +36,42 @@ public class HomeController implements Initializable {
     private TextField txtKey;
 
     @FXML
-    private Label labelNumber;
+    private TextField txtSize;
 
     @FXML
-    private TextField txtPassword;
-    
+    private TextField txtNewPassword;
+
     private int passwordSize;
-    
+    private final boolean[] option = new boolean[5];
+
+    public void onBtCreatePassword() {
+        txtNewPassword.setText("");
+        if (rbActiveKey.isSelected()) {
+            if (txtKey.getText().isEmpty()) {
+                txtKey.setStyle("-fx-border-color: red;");
+            }
+        } else {
+            option[0] = rbABC.isSelected();
+            option[1] = rbabc.isSelected();
+            option[2] = rb123.isSelected();
+            option[3] = rbCharacters.isSelected();
+            passwordSize = Integer.parseInt(txtSize.getText());
+            
+            txtNewPassword.setText(generate.passwordWithoutkey(option, passwordSize));
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       initializeNodes();
-    }    
- 
-    private void initializeNodes() {
-        sliderNumber.valueProperty().addListener(new ChangeListener<Number>() {
-           @Override
-           public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
-               passwordSize = (int) sliderNumber.getValue();
-               labelNumber.setText(""+passwordSize);
-           }   
-       });
+        initializeNodes();
     }
-    
+
+    private void initializeNodes() {
+        txtNewPassword.setText(generate.passwordStandard());
+        sliderNumber.valueProperty().addListener((ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+            passwordSize = (int) sliderNumber.getValue();
+            txtSize.setText("" + passwordSize);
+        });
+    }
+
 }
